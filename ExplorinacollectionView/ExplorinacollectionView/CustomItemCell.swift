c6e4b5ec583b3344418c33f06a01b95b688e5cc8
Name <email@example.ru>
2023-04-02 02:17:37 -0400

    first

---
+++ ExplorinacollectionView/ExplorinacollectionView/CustomItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomItemCell.swift
+//  ExplorinacollectionView
+//
+//  Created by OneClick on 2/4/23.
+//
+
+import UIKit
+
+class CustomItemCell: UICollectionViewCell {
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
